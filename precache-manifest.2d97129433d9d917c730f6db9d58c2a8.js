b569118f45e7f572411e1302d630423b62ac44f0
mavrick w <mavwatts@gmail.com>
2020-04-09 19:44:18 -0400

    Updates

---
+++ precache-manifest.2d97129433d9d917c730f6db9d58c2a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e998ce4dcfda48c5862c75c8594c8b50",
+    "url": "/Recipe-Finder/index.html"
+  },
+  {
+    "revision": "5c94c03459251d07562a",
+    "url": "/Recipe-Finder/static/css/main.daab905b.chunk.css"
+  },
+  {
+    "revision": "88f0e4d2e12f23540533",
+    "url": "/Recipe-Finder/static/js/2.d3901c12.chunk.js"
+  },
+  {
+    "revision": "7f36efac70c95fde5c159feb4cd24ff0",
+    "url": "/Recipe-Finder/static/js/2.d3901c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c94c03459251d07562a",
+    "url": "/Recipe-Finder/static/js/main.fd394df2.chunk.js"
+  },
+  {
+    "revision": "fcbe5e79060ddbcc2751",
+    "url": "/Recipe-Finder/static/js/runtime-main.24d19dc3.js"
+  }
+]);
